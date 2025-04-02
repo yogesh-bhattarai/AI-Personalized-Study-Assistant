3a657b3252a7d4d15a4012ddaf45e99230898168
Yogesh <yogesh.bhattarai633@gmail.com>
2025-04-02 23:17:57 +0545

    error in code

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import re
 from pdfminer.high_level import extract_text
+import pdf2image
+import pytesseract
 
 def get_text_from_pdf(pdf_path):
     return extract_text(pdf_path)
 
+def extract_text_ocr(pdf_path):
+    images= pdf2image.convert_from_path(pdf_path)
+    text= "\n".join([pytesseract.image_to_string(img)for img in images])
+    return text
+h1=extract_text_ocr(r"C:\Users\yogass\Desktop\Study_Assistance\sample syallubus.pdf")
+print(h1)
 
 result= get_text_from_pdf(r"C:\Users\yogass\Desktop\MYCV.pdf")
 #print(result)
